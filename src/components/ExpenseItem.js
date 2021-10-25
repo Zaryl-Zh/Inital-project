d8cccab4b8a3c122bb7dc7017fe34e044ad26842
Zaryl-Zh <zaryl.abdrazakova@gmail.com>
2021-10-25 12:18:09 +0600

    init project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+
+function ExpenseItem (props) {
+    let expenseDate = props.date;
+    let expenseTitle = props.title;
+    let expenseAmount = props.amount;
+ console.log(props);
+ 
+    return (
+        <div className='expense-item'>
+         <ExpenseDate date={expenseDate}/>
+           <div className='expense-item__description'>
+            <h2>{expenseTitle}</h2>
+           <div className='expense-item__price '>${expenseAmount}</div>
+          </div>
+        </div>
+    )
+} 
+
+export default ExpenseItem;
